526d67c67c2e75a46f590044d4561e0a5f4ea067
openlockPPP <39940165+openlockPPP@users.noreply.github.com>
2023-07-24 11:06:28 +0800

    ning
    add my prodyct

---
+++ README.md
@@ -0,0 +1,2 @@
+# mvitality_books
+ 学术研究-product
---
+++ android/app/src/main/java/com/example/mvitality_books/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.mvitality_books;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
